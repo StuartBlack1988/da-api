4dad4cb6e53f3d0827070a648b86863edc27b00f
StuartBlack1988 <stuart@swbza.co.za>
2025-05-14 21:49:32 +0200

    migrations 5

--- src/Migration/routes.php
+++ src/Migration/routes.php
@@ -5,6 +5,15 @@ use DietitianAssist\Migration\MigrationController;
 // Initialize the migration controller
 $migrationController = new MigrationController($db);
 
+// Debug route
+$router->get('/api/migrations/debug', function() {
+    echo json_encode([
+        'message' => 'Migration routes are working',
+        'path' => __DIR__,
+        'files' => glob(__DIR__ . '/*.php')
+    ]);
+});
+
 // Route for running migrations
 $router->post('/api/migrations/run', function() use ($migrationController) {
     $response = $migrationController->runMigrations();
